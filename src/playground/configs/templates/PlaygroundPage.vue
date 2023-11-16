2871163dab32e995adf05b9091c88f2380d7f740
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-11-15 18:18:56 -0800

    Update template

--- src/playground/configs/templates/PlaygroundPage.vue
+++ src/playground/configs/templates/PlaygroundPage.vue
@@ -1,167 +1,211 @@
 <template>
 	<v-col
-		class="offset-4 mt-15 pt-15"
+		class="text-center"
+		cols="12"
+	>
+		<v-btn
+			class="me-1"
+			@click="VDateFieldProps.disabled = !VDateFieldProps.disabled"
+		>
+			Disabled {{ VDateFieldProps.disabled }}
+		</v-btn>
+		<v-btn @click="VDateFieldProps.loading = !VDateFieldProps.loading">
+			Loading {{ VDateFieldProps.loading }}
+		</v-btn>
+	</v-col>
+
+	<v-col
+		class="text-center"
+		cols="12"
+	>
+		{{ dateModel }}
+	</v-col>
+
+	<v-col
+		class="offset-4"
 		cols="4"
 	>
 		<v-card elevation="5">
 			<v-container>
-				<VColorField
-					v-model="color"
-					:append-icon="VColorFieldProps.appendIcon"
-					:append-inner-icon="VColorFieldProps.appendInnerIcon"
-					:canvasHeight="VColorFieldProps.canvasHeight"
-					:cardFieldWidth="VColorFieldProps.cardFieldWidth"
-					:cardOffsetX="VColorFieldProps.cardOffsetX"
-					:cardOffsetY="VColorFieldProps.cardOffsetY"
-					:cardPadding="VColorFieldProps.cardPadding"
-					:cardProps="VColorFieldProps.cardProps"
-					:clearable="VColorFieldProps.clearable"
-					:color="VColorFieldProps.color"
-					:color-picker-props="VColorFieldProps.colorPickerProps"
-					:density="VColorFieldProps.density"
-					:dotSize="VColorFieldProps.dotSize"
-					:hideCanvas="VColorFieldProps.hideCanvas"
-					:hideInputs="VColorFieldProps.hideInputs"
-					:hideSliders="VColorFieldProps.hideSliders"
-					:hint="VColorFieldProps.hint"
-					:hint-align="VColorFieldProps.hintAlign"
-					:icon-hover-color="VColorFieldProps.iconHoverColor"
-					:iconSize="VColorFieldProps.iconSize"
-					:label="VColorFieldProps.label"
-					:messages="VColorFieldProps.messages"
-					:mode="VColorFieldProps.mode"
-					:modes="VColorFieldProps.modes"
-					:name="VColorFieldProps.name"
-					:open="VColorFieldProps.open"
-					:persistent-hint="VColorFieldProps.persistentHint"
-					:persistent-placeholder="VColorFieldProps.persistentPlaceholder"
-					:pip="VColorFieldProps.pip"
-					:pipBorder="VColorFieldProps.pipBorder"
-					:pipBorderRadius="VColorFieldProps.pipBorderRadius"
-					:pipHeight="VColorFieldProps.pipHeight"
-					:pipIcon="VColorFieldProps.pipIcon"
-					:pipRadius="VColorFieldProps.pipRadius"
-					:pipSlot="VColorFieldProps.pipSlot"
-					:placeholder="VColorFieldProps.placeholder"
-					:prepend-icon="VColorFieldProps.prependIcon"
-					:prepend-inner-icon="VColorFieldProps.prependInnerIcon"
-					:readonly="VColorFieldProps.readonly"
-					:readonlyInput="VColorFieldProps.readonlyInput"
-					:required="VColorFieldProps.required"
-					:showSwatches="VColorFieldProps.showSwatches"
-					:swatches="VColorFieldProps.swatches"
-					:swatchesMaxHeight="VColorFieldProps.swatchesMaxHeight"
-					:theme="VColorFieldProps.theme"
-					:variant="VColorFieldProps.variant"
+				<VDateField
+					v-model="dateModel"
+					:allowedDates="VDateFieldProps.allowedDates"
+					:append-icon="VDateFieldProps.appendIcon"
+					:append-inner-icon="VDateFieldProps.appendInnerIcon"
+					:cardFieldWidth="VDateFieldProps.cardFieldWidth"
+					:cardOffsetX="VDateFieldProps.cardOffsetX"
+					:cardOffsetY="VDateFieldProps.cardOffsetY"
+					:cardPadding="VDateFieldProps.cardPadding"
+					:cardProps="VDateFieldProps.cardProps"
+					:clearable="VDateFieldProps.clearable"
+					:color="VDateFieldProps.color"
+					:date-picker-props="VDateFieldProps.datePickerProps"
+					:density="VDateFieldProps.density"
+					:disabled="VDateFieldProps.disabled"
+					:elevation="VDateFieldProps.elevation"
+					:format="VDateFieldProps.format"
+					:header="VDateFieldProps.header"
+					:height="VDateFieldProps.height"
+					:hideHeader="VDateFieldProps.hideHeader"
+					:hideWeekdays="VDateFieldProps.hideWeekdays"
+					:hint="VDateFieldProps.hint"
+					:hint-align="VDateFieldProps.hintAlign"
+					:icon-hover-color="VDateFieldProps.iconHoverColor"
+					:iconSize="VDateFieldProps.iconSize"
+					:label="VDateFieldProps.label"
+					:loading="VDateFieldProps.loading"
+					:max="VDateFieldProps.max"
+					:maxHeight="VDateFieldProps.maxHeight"
+					:maxWidth="VDateFieldProps.maxWidth"
+					:messages="VDateFieldProps.messages"
+					:min="VDateFieldProps.min"
+					:minHeight="VDateFieldProps.minHeight"
+					:minWidth="VDateFieldProps.minWidth"
+					:modeIcon="VDateFieldProps.modeIcon"
+					:month="VDateFieldProps.month"
+					:multiple="VDateFieldProps.multiple"
+					:name="VDateFieldProps.name"
+					:nextIcon="VDateFieldProps.nextIcon"
+					:open="VDateFieldProps.open"
+					:persistent-hint="VDateFieldProps.persistentHint"
+					:persistent-placeholder="VDateFieldProps.persistentPlaceholder"
+					:pickerBgColor="VDateFieldProps.pickerBgColor"
+					:pickerColor="VDateFieldProps.pickerColor"
+					:pickerTag="VDateFieldProps.pickerTag"
+					:placeholder="VDateFieldProps.placeholder"
+					:prepend-icon="VDateFieldProps.prependIcon"
+					:prepend-inner-icon="VDateFieldProps.prependInnerIcon"
+					:prevIcon="VDateFieldProps.prevIcon"
+					:readonly="VDateFieldProps.readonly"
+					:readonlyInput="VDateFieldProps.readonlyInput"
+					:required="VDateFieldProps.required"
+					:showAdjacentMonths="VDateFieldProps.showAdjacentMonths"
+					:showWeek="VDateFieldProps.showWeek"
+					:theme="VDateFieldProps.theme"
+					:title="VDateFieldProps.title"
+					:variant="VDateFieldProps.variant"
+					:viewMode="VDateFieldProps.viewMode"
+					@update:month="console.log('UPDATE month', $event)"
+					@update:viewMode="console.log('UPDATE viewMode', $event)"
+					@update:year="console.log('UPDATE year', $event)"
 				>
-					<!-- <template #prepend="{ toggleColorPicker }">
-							<v-icon
-								icon="$vuetify"
-								@click="toggleColorPicker"
-							/>
-						</template> -->
+					<!-- <template #title>
+						Hello World
+					</template> -->
 
-					<!-- <template #prepend-inner="{ toggleColorPicker }">
-							<v-icon
-								icon="$vuetify"
-								@click="toggleColorPicker"
-							/>
-						</template> -->
+					<!-- <template #header="props">
+						<div class="v-picker__header">
+							<div class="v-date-picker-header">
+								<div class="v-date-picker-header__content">{{ props.header }}</div>
+							</div>
+						</div>
+					</template> -->
 
-					<!-- <template #append-inner="{ toggleColorPicker }">
-							<v-icon
-								icon="$vuetify"
-								@click="toggleColorPicker"
-							/>
-						</template> -->
-
-					<!-- <template #append="{ toggleColorPicker }">
-							<v-icon
-								icon="$vuetify"
-								@click="toggleColorPicker"
-							/>
-						</template> -->
-
-					<!-- <template #label>
-							Color
-						</template> -->
-
-				</VColorField>
+					<!-- <template #actions>
+						<v-btn class="me-1">Cancel</v-btn>
+						<v-btn>Ok</v-btn>
+					</template> -->
+				</VDateField>
 			</v-container>
 		</v-card>
 	</v-col>
+
+
+	<!-- <v-col cols="12">
+		<v-date-picker
+			v-model="testDate"
+			multiple
+		></v-date-picker>
+	</v-col>
+	<v-col cols="12">
+		{{ testDate }}
+	</v-col> -->
 </template>
 
 <script setup>
-const color = ref(null);
+// const dateModel = ref(new Date('11-20-2023'));
+// const dateModel = ref('11-20-2023');
+// const dateModel = ref([new Date('2023/11/20')]);
+const dateModel = ref('2023/11/20, 2023/11/21, 2023/11/22, 2023/11/23');
+// const dateModel = ref(undefined);
+// const dateModel = ref(new Date('2023-11-20'));
+// const testDate = ref([new Date('2023/11/20')]);
 
-const colorPickerProps = {
-	canvasHeight: 150,
-	dotSize: 10,
-	hideCanvas: false,
-	hideInputs: false,
-	hideSliders: false,
-	mode: 'rgba',
-	modes: ['rgb', 'rgba', 'hsl', 'hsla', 'hex', 'hexa'],
-	showSwatches: true,
-	// swatches: [
-	// 	['#FF0000', '#AA0000', '#550000'],
-	// 	['#FFFF00', '#AAAA00', '#555500'],
-	// 	['#00FF00', '#00AA00', '#005500'],
-	// 	['#00FFFF', '#00AAAA', '#005555'],
-	// 	['#0000FF', '#0000AA', '#000055'],
-	// ],
-	swatchesMaxHeight: 150,
+const datePickerProps = {
+	// allowedDates: ['2023/11/20', '2023/11/21'],
+	header: 'Select Date',
+	height: undefined,
+	hideHeader: false,
+	hideWeekdays: false,
+	max: undefined,
+	maxHeight: undefined,
+	maxWidth: undefined,
+	min: undefined,
+	minHeight: undefined,
+	minWidth: undefined,
+	modeIcon: '$subgroup',
+	month: 3,
+	multiple: true,
+	nextIcon: '$next',
+	pickerBgColor: undefined,
+	pickerColor: undefined,
+	pickerTag: 'div',
+	prevIcon: '$prev',
+	showAdjacentMonths: false,
+	showWeek: false,
+	title: '$vuetify.datePicker.title',
+	viewMode: 'month',
 };
 
-const VColorFieldProps = ref({
-	...colorPickerProps,
-	appendIcon: undefined,
-	appendInnerIcon: undefined,
-	cardFieldWidth: false,
-	cardOffsetX: 0,
-	cardOffsetY: 5,
-	cardPadding: 4,
-	cardProps: {
-		border: false,
-		color: '',
-		elevation: 5,
-		loading: false,
-		tag: 'div',
-	},
+const datePickerComponentProps = {
+};
+
+const VDateFieldProps = ref({
+	...datePickerProps,
+	// appendIcon: '$vuetify',
+	// appendInnerIcon: '$vuetify',
+	// cardFieldWidth: false,
+	// cardOffsetX: 0,
+	// cardOffsetY: 5,
+	// cardPadding: 4,
+	// cardProps: {
+	// 	border: false,
+	// 	color: '',
+	// 	elevation: 9,
+	// 	loading: false,
+	// 	tag: 'div',
+	// },
+	// color: '',
 	clearable: true,
-	color: '',
-	colorPickerProps: {
-		dotSize: 30,
-		// mode: 'hex',
-		// modes: ['hex'],
-		// showSwatches: true,
-	},
-	density: undefined,
-	hint: undefined,
-	hintAlign: undefined,
-	iconHoverColor: undefined,
-	iconSize: 'default',
-	label: undefined,
-	messages: undefined,
-	// messages: 'This is a string message',
-	// messages: ['This is a message', 'This is another message'],
-	name: 'color',
-	open: 'bottom left',
-	persistentHint: true,
-	persistentPlaceholder: false,
-	pip: false,
-	// pipBorder: 'none',
-	// pipBorderRadius: '50%',
-	pipIcon: undefined,
-	pipSlot: undefined,
-	placeholder: 'Select Color',
-	prependIcon: undefined,
-	prependInnerIcon: undefined,
+	datePickerProps: datePickerComponentProps,
+	density: 'default',
+	disabled: false,
+	// elevation: 9,
+	format: 'YYYY/MM/DD',
+	// hint: undefined,
+	// hintAlign: undefined,
+	// iconHoverColor: undefined,
+	// iconSize: 'default',
+	label: 'Date',
+	loading: false,
+	// messages: undefined,
+	// // messages: 'This is a string message',
+	// // messages: ['This is a message', 'This is another message'],
+	// name: 'color',
+	// open: 'bottom left',
+	// persistentHint: true,
+	// persistentPlaceholder: false,
+	// placeholder: 'Select Date',
+	// prependIcon: '$vuetify',
+	// prependInnerIcon: '$vuetify',
 	readonly: undefined,
 	readonlyInput: undefined,
 	required: false,
-	theme: 'dark',
-	variant: 'filled',
+	// theme: 'light',
+	variant: 'outlined',
 });
+
+// console.log(VDateFieldProps.value);
+
+
 </script>
